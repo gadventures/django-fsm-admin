e3490c7202de509cbddeb3a29f72413da323e21f
Nagyman <craig.nagy@gmail.com>
2014-03-12 00:42:00 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-fsm-admin',
-    version='0.0.1',
+    version='0.0.2',
     author='G Adventures',
     author_email='software@gadventures.com',
     url='https://github.com/gadventures/django-fsm-admin',
