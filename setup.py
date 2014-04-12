1f74eea52003654357a65e13a571255670e6966f
Nagyman <craig.nagy@gmail.com>
2014-04-12 10:48:41 -0400

    Bump version to redistribute on PyPi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-fsm-admin',
-    version='0.0.3',
+    version='0.0.4',
     author='G Adventures',
     author_email='software@gadventures.com',
     url='https://github.com/gadventures/django-fsm-admin',
