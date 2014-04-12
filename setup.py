768024478ccb3baddbe6ce5438899415aab35929
Nagyman <craig.nagy@gmail.com>
2014-04-12 10:17:04 -0400

    Include package data for proper pypi distribution
    Fixes #4
    
    Thanks to @titusz for the report.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='django-fsm-admin',
-    version='0.0.2',
+    version='0.0.3',
     author='G Adventures',
     author_email='software@gadventures.com',
     url='https://github.com/gadventures/django-fsm-admin',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Django>=1.6',
         'django-fsm>=1.5.1',
     ],
-
 )
