7815b1df8361c51929ba6975c897cd084012576c
Nagyman <craig.nagy@gmail.com>
2014-04-14 08:34:55 -0400

    Minor version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-fsm-admin',
-    version='0.0.4',
+    version='0.0.5',
     author='G Adventures',
     author_email='software@gadventures.com',
     url='https://github.com/gadventures/django-fsm-admin',
